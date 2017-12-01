e39a5751d76505a5818f0ce7661b368d0803d2ba
Pierre-Marie <p.dartus@salesforce.com>
2017-12-01 07:39:31 -0800

    Add proper playground

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -6,24 +6,43 @@ const SRC_DIR = path.resolve(__dirname, '..', 'src');
 const DIST_DIR = path.resolve(__dirname, '..', 'dist');
 
 module.exports = {
-    entry: path.resolve(SRC_DIR, 'playground', 'index.js'),
+    entry: path.resolve(SRC_DIR, 'playground', 'index.ts'),
 
     output: {
         filename: '[name].js',
         path: path.resolve(DIST_DIR, 'playground'),
     },
-    
+
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(DIST_DIR, 'playground'),
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: [ '.ts', '.js' ]
     },
 
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin([path.resolve(DIST_DIR, 'playground')], {
+            allowExternal: true,
+        }),
         new HtmlWebpackPlugin({
             title: 'tlox',
-            template: path.resolve(SRC_DIR, 'playground', 'index.html')
+            template: path.resolve(SRC_DIR, 'playground', 'index.html'),
         }),
     ],
-    
 };
---
+++ src/playground/index.ts
@@ -0,0 +1,91 @@
+import Runner from '../core/runner';
+
+declare global {
+    interface Window { 
+        require: any;
+        monaco: any;
+    }
+}
+
+const PRESETS = [{
+    name: 'Hello world',
+    value: 'print "Hello World!";',
+}, {
+    name: 'Function',
+    value: 'fun sayHi(first, last) {\n  print "Hello " + first + " " + last + ", nice to meet you";\n}\n\nsayHi("John", "Doe");',
+}];
+
+const presetSelect = document.querySelector('#preset-select') as HTMLSelectElement;
+const runButton = document.querySelector('#run-button')!;
+const editorContainer = document.querySelector('#editor-container')!;
+const logContainer = document.querySelector('#log-container')!;
+
+for (let preset of PRESETS) {
+    const option = document.createElement('option');
+    
+    option.text = preset.name;
+    option.value = preset.value;
+
+    presetSelect.appendChild(option);
+}
+
+window.require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.10.1/min/vs' }});
+
+window.require(['vs/editor/editor.main'], () => {
+    const editor = window.monaco.editor.create(editorContainer, {
+        value: presetSelect.value,
+        language: 'lox',
+    });
+
+    const log = window.monaco.editor.create(logContainer, {
+        value: '',
+
+        lineNumbers: false,
+        scrollBeyondLastLine: false,
+        
+        readOnly: true,
+        contextmenu: false,
+        hideCursorInOverviewRuler: true,
+
+        minimap: {
+            enabled: false
+        },
+
+        theme: 'vs-dark',
+    });
+    
+    presetSelect.addEventListener('change', () => {
+        editor.setValue(presetSelect.value);
+    });
+
+    runButton.addEventListener('click', () => {
+        const value = editor.getValue();
+
+        const logLines: string[] = [];
+        const addLogLine = function(msg: string) {
+            logLines.push(msg);
+            log.setValue(logLines.join('\n'));
+        }
+
+        const runner = new Runner({
+            debug(msg: string) { 
+                console.debug(msg)
+                addLogLine(`🔎 ${msg}`);
+            },
+            log(msg: string) {
+                console.log(msg)
+                addLogLine(`  ${msg}`);
+            },
+            warning(msg: string) {
+                console.warn(msg)
+                addLogLine(`⚠️ ${msg}`);
+            },
+            error(msg: string) {
+                console.error(msg);
+                addLogLine(`️❌ ${msg}`);
+            },
+        });
+
+        runner.run(value);
+    });
+});
